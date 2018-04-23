3c5e5ca247e7f5d88f05de042b982a5cc63fb085
Yosafat Bama <yosafatbama.arintoko@gmail.com>
2018-04-23 20:15:01 +0700

    edit page story

--- pages/Pages/Spa/page-story.js
+++ pages/Pages/Spa/page-story.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
-
+import { Col, Card, Row, CardTitle } from 'react-materialize'
 class OurStory extends Component {
     render() {
         return (
-            <div className='wrapper_' style={{
+            <div style={{
                 width: "100%",
-                height: "100vh",
+                height: "100%",
                 position: 'static',
                 backgroundColor: '#FFFFFF'
             }}>
-                <span>
-                    Our Story
-                    </span>
+                <Col className='story_top' m={12} s={12}>
+                    <Row className='child_' style={{ paddingTop: 100 }}>
+
+                        <Col m={4} s={12}>
+                            <Card style={{ borderRadius: 10, borderWidth: 1, borderColor: '#9E9E9E', height:200 }} className='_card' textClassName='black-text' title='Web Development'>
+                            <span className='text_'>We develop responsive web using ReactJs. ReactJs is component based. Components: The future of web development.</span>
+                        </Card>
+                        </Col>
+                        <Col m={4} s={12}>
+                            <Card style={{ borderRadius: 10, borderWidth: 1, borderColor: '#9E9E9E', height:200 }} className='_card' textClassName='white-text' title='Design'>
+                                I am a very simple card.
+                        </Card>
+                        </Col>
+                        <Col m={4} s={12}>
+                            <Card style={{ borderRadius: 10, borderWidth: 1, borderColor: '#9E9E9E', height:200 }} className='_card' textClassName='black-text' title='Mobile App'>
+                                <span className='text_'>We create app using React Native framework. With React Native, you don't build a "mobile web app", an "HTML5 app", or a "hybrid app". You build a real mobile app that's indistinguishable from an app built using Objective-C or Java.</span>
+                        </Card>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col className='story_below' m={12} s={12}>
+                    <Row className='child' style={{ textAlign: 'center' }}>
+                        <h2 className='text'>CREATED BY EXPERTS</h2>
+                        <p className='text_'>We believe in a diverse range of talent and personality brings creative skills and ideas to the web.</p>
+                    </Row>
+                </Col>
+
             </div>
         );
     }
